b1e61c2474999a9f2613e36373105e3a7e680553
Mike Wieder <mike-wieder@hotmail.de>
2016-11-21 22:04:34 +0100

    created project

---
+++ src/verteilte_systeme_aufgabe5_client/Aufgabe5_Client.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verteilte_systeme_aufgabe5_client;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mike
+ */
+public class Aufgabe5_Client {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String host = null;
+        try {
+            Registry reg = LocateRegistry.getRegistry(host);
+            MemInterface mem = (MemInterface) Naming.lookup("rmi://localhost/Mem");
+            mem.put(5);
+            System.out.println(mem.get());
+        } catch (Exception ex) {
+            Logger.getLogger(Aufgabe5_Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
